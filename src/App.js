6739ce556940f743689f0d75bc88ad2b70c68032
Sachin Saxena <saxenas964@gmail.com>
2023-08-07 23:49:39 +0530

    simple dark mode button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
+import About from "./components/About";
 import Form from "./components/Form";
 import NavBar from "./components/NavBar";
 function App() {
   return (
     <>
       <NavBar title="Text Utils" about="About Us" />
-      <Form title="Enter your text" />
+      {/* <Form title="Enter your text" /> */}
+      <About />
     </>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -7,30 +7,52 @@ export default function Form(props) {
   const handleLowClick = () => {
     setText(text.toLowerCase());
   };
+  const handleClearClick = () => {
+    setText("");
+  };
   const handleChange = (event) => {
     setText(event.target.value);
   };
-  const [text, setText] = useState("Enter text here");
+  const [text, setText] = useState("");
   return (
-    <div className="container my-3">
-      <div className="mb-3">
-        <label htmlFor="exampleFormControlTextarea1" className="form-label">
-          {props.title}
-        </label>
-        <textarea
-          className="form-control"
-          id="exampleFormControlTextarea1"
-          rows="8"
-          value={text}
-          onChange={handleChange}
-        ></textarea>
+    <>
+      <div className="container my-3">
+        <div className="mb-3">
+          <label
+            htmlFor="exampleFormControlTextarea1"
+            className="form-label fw-bold"
+          >
+            {props.title}
+          </label>
+          <textarea
+            className="form-control"
+            id="exampleFormControlTextarea1"
+            rows="8"
+            value={text}
+            onChange={handleChange}
+          ></textarea>
+        </div>
+        <button className="btn btn-primary" onClick={handleUpClick}>
+          Convert to UpperCase
+        </button>
+        <button className="btn btn-primary ms-3" onClick={handleLowClick}>
+          Convert to LowerCase
+        </button>
+        <button className="btn btn-primary ms-3" onClick={handleClearClick}>
+          Reset
+        </button>
       </div>
-      <button className="btn btn-primary" onClick={handleUpClick}>
-        Convert to UpperCase
-      </button>
-      <button className="btn btn-primary ms-3" onClick={handleLowClick}>
-        Convert to LowerCase
-      </button>
-    </div>
+      <div className="container">
+        <label
+          htmlFor="exampleFormControlTextarea2"
+          className="form-label fw-bold"
+        >
+          Text Summary
+        </label>
+        <p>
+          {text.trim().split(" ").length} words & {text.length} characters{" "}
+        </p>
+      </div>
+    </>
   );
 }
